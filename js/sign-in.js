517401a12f1fd1ee92366be7da4fd2b18a708a88
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-07-13 19:45:12 -0500

    Create logIn/logOut functions and and object to store all users/current user

--- js/sign-in.js
+++ js/sign-in.js
@@ -8,8 +8,30 @@
 function handleButton() {
   let nameValue = document.getElementById("name").value;
   let emailValue = document.getElementById("email").value;
-  let user = new User(nameValue, emailValue);
-  localStorage.setItem(userKey, JSON.stringify(user));
+  logIn(nameValue,emailValue);
   let greeting = document.getElementById("greeting");
   greeting.textContent = `Welcome ${nameValue}`;
+}
+
+/**
+ * Takes the logIn information and retrieves that user from memory or creates a new user
+ * @param {string} userName - Name that appears for the user
+ * @param {string} email - Unique email associated with this account
+ */
+function logIn(userName,email) {
+  let userLogIn;
+  let isNewUser = true;
+  for (let i = 0; i < userDataObject.allUsers.length; i++) {
+    if (userDataObject.allUsers[i].email === email) {
+      isNewUser = false;
+      userLogIn = userDataObject.allUsers[i];
+      break;
+    }
+  }
+  if (isNewUser) {
+    userLogIn = new User(userName,email);
+    userDataObject.allUsers.push(userLogIn);
+  }
+  userDataObject.currentUser = userLogIn;
+  localStorage.setItem(userKey, JSON.stringify(userDataObject));
 }
--- js/user.js
+++ js/user.js
@@ -2,15 +2,43 @@
 * @file user.js
 * @date 2022-07-13
 * @author dsfrey
-* @description Constructor function for user objects
+* @description Controls for user objects
 */
 'use strict';
 
 const userKey="userKey";
+let userDataObject = new UserData();
 
+/**
+ * Constructs an object to contain all user data, either pulling from storage or initializing with empty values
+ */
+function UserData() {
+  if (localStorage.getItem(userKey) === null) {
+    this.allUsers = [];
+    this.currentUser = null;
+  } else {
+    let storedData = JSON.parse(localStorage.getItem(userKey));
+    this.allUsers = storedData.allUsers;
+    this.currentUser = storedData.currentUser;
+  }
+}
 
+/**
+ * Removes the value from current user
+ */
+function logOut() {
+  userDataObject.currentUser = null;
+  localStorage.setItem(userKey, JSON.stringify(userDataObject));
+}
+
+/**
+ * Creates a new user object
+ * @param {string} userName - Name that appears for the user
+ * @param {string} email - Unique email associated with this account
+ */
 function User(userName,email) {
   this.Name = userName;
   this.email = email;
   this.tokens = 0;
+  this.bookshelf = [];
 }
